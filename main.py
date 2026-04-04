1aea271c9c0b2424f3f0ea7245e20797ba25f691
Alexey <myk1n.alex@gmail.com>
2026-04-04 16:51:42 +0300

    Start of data_preprocessing

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hello, World!")
+import pandas as pd
+import numpy as np
+import torch
+
+df = pd.read_csv("churn_modeling.csv")
+print(df.head())
+df = df.drop(columns=["RowNumber", "CustomerId", "Surname"])
+cols = df.columns
+df = np.array(df)
+
+for col in cols:
+
+
+
+
+# data = torch.from_numpy(np.array(df))
+# print(data[0])
