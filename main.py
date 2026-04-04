29320802b283399e4f096f4f9884f21f58d8c6a7
Alexey <myk1n.alex@gmail.com>
2026-04-04 18:26:25 +0300

    Data has been understood

--- main.py
+++ main.py
@@ -3,32 +3,15 @@ import numpy as np
 import torch
 
 df = pd.read_csv("churn_modeling.csv")
+df = df.drop(columns=["RowNumber", "CustomerId", "Surname"]) #Очевидно ненужные признаки
 print(df.head())
-df = df.drop(columns=["RowNumber", "CustomerId", "Surname"])
+df = pd.get_dummies(df, columns=['Geography', 'Gender'], dtype=int) #Превращаем эти колонки в one-hot кодирование
 cols = df.columns
-print(cols)
-X = np.array(df)[:, :-1]
-y = np.array(df)[:, -1]
 
-for x, col in zip(X[0], cols[:-1]):
-    print(f'Значение колонки {col}: {x}')
-
-#Видно, что 0, 3, 5, 9 - точно числовые колонки
-#Видно, что 1, 2, 7, 8 - точно категориальные колонки
-#Посмотрим, что с остальными: 4, 6
-
-idxs_to_check = [4,6]
-
-for idx in idxs_to_check:
-    print(f"Множество значений колонки {cols[idx]}: {set(X[:, idx])}")
-
-#Видно что обе колонки можно считать категориальными,
-#хотя по факту они числовые (срок владения и число продуктов)
-#Хотя, наверное я путаю понятия категориальных и не категориальных признаков.
-#Так то везде кроме колонок 1 и 2 признаки числовые и с ними надо работать просто как с числом.
-#Зато потренировался с индексацией и срезами...
+X = df.drop(columns=['Exited']).values
+y = df['Exited'].values
+#теперь мы имеем преобразованные признаки.
+print(X[0])
 
 
 
-# data = torch.from_numpy(np.array(df))
-# print(data[0])
