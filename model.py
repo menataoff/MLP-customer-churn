574876d9b74de4b5ff44a725d8aaab694382de6b
Alexey <myk1n.alex@gmail.com>
2026-04-13 22:23:09 +0300

    Ready fit

--- model.py
+++ model.py
@@ -16,11 +16,12 @@ class MyDataset(Dataset):
 class ModelMLP(nn.Module):
     def __init__(self, input_dim):
         super().__init__()
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.to(self.device)
         self.loss_history = []
         self.net = nn.Sequential(nn.Linear(input_dim, 32),
                                  nn.ReLU(),
-                                 nn.Linear(32, 2),
-                                 nn.Sigmoid())
+                                 nn.Linear(32, 1)) #почему лучше без сигмоиды? потому что численно стабильнее
         self.opt_dict = {
             "Adam": optim.Adam,
             "RMSprop": optim.RMSprop,
@@ -30,11 +31,19 @@ class ModelMLP(nn.Module):
     def forward(self, x):
         return self.net(x)
     def fit(self, X_train, y_train, opt_name="Adam", epochs=128, batch_size=32, **opt_kwargs):
+        self.train()
         data_train = DataLoader(MyDataset(X_train, y_train), batch_size=batch_size, shuffle=True, drop_last=False)
         optimizer = self.opt_dict[opt_name](self.parameters(), **opt_kwargs)
-        #self.train() #переход в режим тренировки
-        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        for epoch in tqdm(range(epochs), desc='Fitting model'):
-            pass
+        criterion = nn.BCEWithLogitsLoss()
+        for epoch in tqdm(range(epochs)):
+            for X_batch, y_batch in data_train:
+                X_batch, y_batch = X_batch.to(self.device), y_batch.to(self.device)
+                optimizer.zero_grad()
+                y_pred = self.forward(X_batch)
+                loss = criterion(y_pred, y_batch)
+                loss.backward()
+                self.loss_history.append(loss.item())
+                optimizer.step()
+
 
 
